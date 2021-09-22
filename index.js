ff56ed6b9458b3733f9a96facfb54ae3673ff3f1
henryiscoder <qtuncay70@gmail.com>
2021-09-22 17:20:27 +0300

    Update 1

--- index.js
+++ index.js
@@ -15,6 +15,36 @@ app.get("/zcod29DofuWp",(req,res)=>{
 
 app.post("/search",(req, res)=>{
   let data = req.body.key
+  if(data === "macros"){
+      res.redirect("users")
+  } else {
+      res.redirect("/")
+  }
+})
+
+app.get("/users",(req,res)=>{
+    res.render("macros")
+})
+
+app.get("/login",(req,res)=>{
+    res.render("login")
+})
+
+app.post("/login",(req,res)=>{
+    let { username, password } = req.body
+    if(username === "sams3p1ol_" && password === "mrrobot31"){
+        res.redirect("/dashboard")
+    } else {
+        res.redirect("/login")
+    }
+})
+
+app.get("/dashboard",(req,res)=>{
+    res.render("dashboard")
+})
+
+app.get("/zcod29DofuWp",(req, res) =>{
+    res.render("key")
 })
 
 app.listen(8080, () => console.log("Listening for requests on http://localhost:8080"))
